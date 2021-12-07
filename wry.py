0633fff026674e282f92fd9155b5a394ca93a7d9
Dr. Zuipperpips Beiuet <arturokfya@gmail.com>
2021-12-07 13:45:12 -0300

    fixed the code. Gave up on dumb idea of trying to lazyify everything. Seems to be running now except for a small bug

--- wry.py
+++ wry.py
@@ -34,17 +34,12 @@ logger.setLevel(logging.DEBUG)
 #é importante notar que, nesse arquivo, eu não tenho intenção de me ater as convenções de codigo pythonico, mas estou programando com paradima funcional. Isso pois estou estudando linguagens funcionais (haskell) e alem de apresentarem uma alternativa mais segura, eficiente de codigo, preciso de treino nelas.
 
 def neural_net_function(activation_fns, parms, x):
-    first_layer, second_layer = (lambda x: activation_fn(parm, x) for parm, activation_fn in zip(parms, activation_fns))
-    return second_layer(first_layer(x))
+    layers = (lambda x: activation_fn(parm, x) for parm, activation_fn in zip(parms, activation_fns))
+    activation = x
+    for l in layers:
+        activation = l(activation)
+    return activation
 
-def take_n(n, xs):
-    def inner():
-        for i in range(n):
-            yield from xs
-    while True:
-        next_iter = inner()
-        yield next_iter
-     
 get_parms_shapes = lambda data_sizes: (sh[::-1] for sh in  pairwise(iter(data_sizes)))
 generate_parms = lambda build_fn: lambda shapes: [build_fn(shape=sh) for sh in shapes]
 #---------------------------------------------------
@@ -63,11 +58,11 @@ random.seed(137)
 
 DTYPE = tf.float64 #para garantir a precisão requerida, usamos o maior float disponivel
 DATA_SIZES = (3,10,1)
-ACTIVATION_FUNCTION = lambda parms, x: tf.sigmoid(parms @ x)
+ACTIVATION_FUNCTION = tf.function(lambda parms, x: tf.sigmoid(parms @ x))
 BATCH_SIZE = 64
 NUM_EPOCHS = 5
 RANDOM_PARMS_GENERATOR = lambda shape: tf.random.uniform(shape=shape, dtype=DTYPE, seed=137, minval=-10, maxval=+10)
-LOSS_FUNCTION = lambda errs: tf.mean(tf.square(errs))
+LOSS_FUNCTION = tf.function(lambda errs: np.mean(tf.square(errs)))
 
 #---------------------------------------------------
 
@@ -97,41 +92,44 @@ initial_parms = generate_parms(RANDOM_PARMS_GENERATOR)(shapes)
 print((initial_parms))
 
 logger.info("Building Neural Net...")
-initial_neural_net = lambda x: neural_net_function(repeat(ACTIVATION_FUNCTION), initial_parms, x)
+initial_neural_net = tf.function(lambda x: neural_net_function(repeat(ACTIVATION_FUNCTION), initial_parms, x))
 
-separate_xy = (lambda t: (t[:,1:4].T, t[:,4:,np.newaxis]))
+separate_xy = tf.function((lambda t: (t[:,1:4].T, t[:,4])))
 
 
 def shuffle_dataset(dataset):
-    dataset_copy = copy.deepcopy(dataset)
-    random.shuffle(dataset_copy)
-    return dataset_copy
-
-def get_epochs(n, ds):
-    for i in range(n):
-        yield ds
-    return
+    rand_indexes = random.sample(range(len(dataset)), len(dataset))
+    return dataset[rand_indexes]
+     
 
 def get_batches(n, ds):
-    generator = take_n(n, ds)
-    yield from generator
+    li = 0
+    while True:
+        ui = min(len(ds), li + n)
+        if ui == li: break
+        else:
+            yield ds[li:ui]
+            li = ui
 
+
+@tf.function
 def evaluate_nn(neural_net, loss_function, batch):
     x_train, y_train = separate_xy(batch)
     y_pred = neural_net(x_train)
     errors = y_train - y_pred
     losses = loss_function(errors)
-    return copy.deepcopy(locals())
+    return y_pred, errors, losses
 
 neural_net = initial_neural_net
 epoch_loop = []
+
+
 for epoch in range(NUM_EPOCHS):
     train_ds = shuffle_dataset(train)
     batch_loop = []
     for batch in get_batches(BATCH_SIZE, train_ds):
-        evaluated_batch = np.array([*batch])
-        print(evaluated_batch)
-        res = evaluate_nn(neural_net, LOSS_FUNCTION, evaluated_batch)
+        print(batch)
+        res = evaluate_nn(neural_net, LOSS_FUNCTION, batch)
         print(res)
         batch_loop.append(res)
     epoch_loop.append(batch_loop)
